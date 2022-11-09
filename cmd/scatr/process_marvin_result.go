199d9b567f4c15fcf3172f0a8e56b6f0a8d49903
Akshit Garg <akshit@deepsource.io>
2022-11-09 16:23:08 +0530

    feat: add marvin processor
    Signed-off-by: Akshit Garg <akshit@deepsource.io>

---
+++ cmd/scatr/process_marvin_result.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	"github.com/shamaton/msgpack"
+	"github.com/spf13/cobra"
+)
+
+var processMarvinResultCmd = &cobra.Command{
+	Use:   "process-marvin-result",
+	Short: "Convert marvin based result into a SCATR result",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		inputFile := os.Getenv("INPUT_FILE")
+		if inputFile == "" {
+			return errors.New("no INPUT_FILE provided")
+		}
+
+		if !verbose {
+			log.SetOutput(io.Discard)
+		}
+
+		ext := path.Ext(inputFile)
+		var isMsgpack bool
+		if ext == ".mpack" {
+			log.Println("MessagePack result detected")
+			isMsgpack = true
+		} else {
+			log.Println("JSON result detected")
+		}
+
+		data, err := unmarshalMarvinResult(isMsgpack, inputFile)
+		if err != nil {
+			return err
+		}
+
+		if !data.IsPassed {
+			return errors.New("The marvin run failed")
+		}
+
+		result := marvinResultToResult(data)
+		resultJSON, err := json.Marshal(result)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(resultJSON))
+
+		return nil
+	},
+}
+
+func init() {
+	processMarvinResultCmd.Flags().BoolVarP(
+		&verbose, "verbose", "v", false,
+		"Use verbose logging",
+	)
+
+	rootCmd.AddCommand(processMarvinResultCmd)
+}
+
+type MarvinLocation struct {
+	Path     string `json:"path" msgpack:"path"`
+	Position struct {
+		Begin struct {
+			Line   int `json:"line" msgpack:"line"`
+			Column int `json:"column" msgpack:"column"`
+		} `json:"begin"`
+		End struct {
+			Line   int `json:"line" msgpack:"line"`
+			Column int `json:"column" msgpack:"column"`
+		} `json:"end" msgpack:"end"`
+	} `json:"position" msgpack:"position"`
+}
+
+type MarvinIssue struct {
+	IssueCode string         `json:"issue_code" msgpack:"issue_code"`
+	IssueText string         `json:"issue_text" msgpack:"issue_text"`
+	Location  MarvinLocation `json:"location" msgpack:"location"`
+}
+
+type MarvinResult struct {
+	Issues   []MarvinIssue `json:"issues" msgpack:"issues"`
+	IsPassed bool          `json:"is_passed" msgpack:"is_passed"`
+}
+
+func unmarshalMarvinResult(isMsgpack bool, file string) (*MarvinResult, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var result MarvinResult
+	if !isMsgpack {
+		err = json.Unmarshal(content, &result)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err = msgpack.Unmarshal(content, &result)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &result, nil
+}
+
+type Result struct {
+	Issues []*Issue `json:"issues"`
+}
+
+type Issue struct {
+	Code     string        `json:"code"`
+	Title    string        `json:"title"`
+	Position IssuePosition `json:"position"`
+}
+
+type IssuePosition struct {
+	File  string    `json:"file"`
+	Start Location  `json:"start"`
+	End   *Location `json:"end"` // end location is a pointer as it may be nil
+}
+
+type Location struct {
+	Line   int `json:"line"`
+	Column int `json:"column"`
+}
+
+func marvinResultToResult(result *MarvinResult) *Result {
+	res := Result{}
+	for _, issue := range result.Issues {
+		res.Issues = append(res.Issues, &Issue{
+			Code:  issue.IssueCode,
+			Title: issue.IssueText,
+			Position: IssuePosition{
+				File: issue.Location.Path,
+				Start: Location{
+					Line:   issue.Location.Position.Begin.Line,
+					Column: issue.Location.Position.Begin.Column,
+				},
+				End: &Location{
+					Line:   issue.Location.Position.End.Line,
+					Column: issue.Location.Position.End.Column,
+				},
+			},
+		})
+	}
+
+	return &res
+}
